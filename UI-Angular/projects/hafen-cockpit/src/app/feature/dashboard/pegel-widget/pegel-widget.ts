953cb08a50444a0f152d05d496182d2d6ac62fdb
Jan Moritz Meyer <j.meyer@implico.de>
2026-07-05 23:04:09 +0200

    way nicer pegel widget visuals with angular material and dto annotations

--- UI-Angular/projects/hafen-cockpit/src/app/feature/dashboard/pegel-widget/pegel-widget.ts
+++ UI-Angular/projects/hafen-cockpit/src/app/feature/dashboard/pegel-widget/pegel-widget.ts
@@ -1,33 +1,64 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 import { AsyncOutlet } from '@cockpit/app/core/async-outlet/async-outlet';
-import { WidgetContainer } from '@cockpit/app/shared/widget/container/container';
+import { WidgetContainer } from '@cockpit/app/shared/widget/container/widget-container';
 import { PegelWidgetService } from './pegel-current.service';
+import { PegelRelationPipe } from './pegel-relation.pipe';
+import { DatePipe } from '@angular/common';
 
 @Component({
 	selector: 'app-pegel-widget',
-	imports: [WidgetContainer, MatProgressBarModule, AsyncOutlet],
+	imports: [
+		WidgetContainer,
+		AsyncOutlet,
+		MatCardModule,
+		MatListModule,
+		PegelRelationPipe,
+		DatePipe,
+	],
 	providers: [PegelWidgetService],
 	template: `
-		<app-widget-container [row]="2" [col]="1" [rowSpan]="1" [colSpan]="4">
-			<ng-container *appAsyncOutlet="pegelService; let pegel = data">
-				<span>{{ pegel.currentMeasurement.value }}</span>
-				<span>{{ pegel.unit }}</span>
-			</ng-container>
+		<app-widget-container [row]="2" [col]="1" [rowSpan]="3" [colSpan]="4">
+			<mat-card>
+				<mat-list *appAsyncOutlet="pegelService; let pegel = data">
+					<mat-list-item>
+						{{ pegel.currentMeasurement.timestamp | date: 'dd.MM.yy HH:mm' }}
+					</mat-list-item>
+					<mat-list-item>
+						<span matListItemTitle>Wasserstand </span>
+						<span matListItemLine
+							>{{ pegel.currentMeasurement.value
+							}}<span class="data-unit">{{ pegel.unit }}</span></span
+						>
+					</mat-list-item>
+					<mat-list-item>
+						<span matListItemTitle>Mnw/Mhw</span>
+						<span>{{ pegel.currentMeasurement.stateMnwMhw | pegelRelation }}</span>
+					</mat-list-item>
+					<mat-list-item>
+						<span matListItemTitle>Nsw/Hsw</span>
+						<span>{{ pegel.currentMeasurement.stateNswHsw | pegelRelation }}</span>
+					</mat-list-item>
+				</mat-list>
 
-			<!-- <img
+				<!-- <img
 				src="https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/d488c5cc-4de9-4631-8ce1-0db0e700b546/W/measurements.png?start=P7D&width=440&height=220"
 			/> -->
+			</mat-card>
 		</app-widget-container>
 	`,
 	changeDetection: ChangeDetectionStrategy.Eager,
 	styles: `
-		app-widget-container {
-			width: 10rem;
-			height: 10rem;
-			display: block;
-			// display: flex;
-			// flex-direction: column;
+		:host {
+			display: contents;
+			app-widget-container {
+				display: block;
+				mat-card {
+					padding: 0.5rem;
+					height: 100%;
+				}
+			}
 		}
 	`,
 })
